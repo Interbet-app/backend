63dba110ac164cb9601814ae33837f614b4f901b
Danrlei Hornke <codingdh@gmail.com>
2022-08-31 13:53:42 -0300

    Update logs.controller.ts

--- src/controllers/logs.controller.ts
+++ src/controllers/logs.controller.ts
@@ -2,26 +2,35 @@ import { Request, Response } from "express";
 import fs from "fs";
 import path from "path";
 
-export async function ShowLogs(req: Request, res: Response) {
-   const { level } = req.query;
-   if (!level) return res.status(400).json({ message: "Nível inexistente!" });
-   if (level !== "error" && level !== "info") return res.status(400).json({ message: "Nível inexistente!" });
+export async function ShowLogs(req: Request, res: Response, next: any) {
+   try {
+      const { level } = req.query;
+      if (!level) return res.status(400).json({ message: "Nível inexistente!" });
+      if (level !== "error" && level !== "info") return res.status(400).json({ message: "Nível inexistente!" });
 
-   const stats = fs.statSync(path.join(__dirname, `../logs/${level}.log`));
-   if (!stats.isFile) return res.status(400).json({ message: "Nível inexistente!" });
-
-
-   const logs = fs.readFileSync(path.join(__dirname, `../logs/${level}.txt`), "utf-8");
-   res.status(200).json({ logs });
+      let log;
+      fs.open(path.join(__dirname, `./logs/${level}.log`), "r", (err, fd) => {
+         if (err?.code === "ENOENT") return res.status(400).json({ message: "Nível inexistente!" });
+         log = fs.readFileSync(fd, "utf-8");
+      });
+      res.status(200).json({ log });
+   } catch (error) {
+      next(error);
+   }
 }
 
-export async function FlushLogs(req: Request, res: Response) {
-   const { level } = req.query;
-   if (!level) return res.status(400).json({ message: "Nível inexistente!" });
-   const stats = fs.statSync(path.join(__dirname, `../../logs/${level}.log`));
-   if (!stats.isFile) return res.status(400).json({ message: "Nível inexistente!" });
-   if (level !== "error" && level !== "info") return res.status(400).json({ message: "Nível inexistente!" });
-   fs.writeFileSync(path.join(__dirname, `../logs/${level}.txt`), "");
-   res.status(200).json({ message: "Logs limpos com sucesso!" });
+export async function FlushLogs(req: Request, res: Response, next: any) {
+   try {
+      const { level } = req.query;
+      if (!level) return res.status(400).json({ message: "Nível inexistente!" });
+      if (level !== "error" && level !== "info") return res.status(400).json({ message: "Nível inexistente!" });
+      fs.open(path.join(__dirname, `./logs/${level}.log`), "w", (err, fd) => {
+         if (err?.code === "ENOENT") return res.status(400).json({ message: "Nível inexistente!" });
+         fs.writeFileSync(fd, "");
+      });
+      res.status(200).json({ message: "Logs limpos com sucesso!" });
+   } catch (error) {
+      next(error);
+   }
 }
 
