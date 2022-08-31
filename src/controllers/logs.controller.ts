9496fdef3da0a0464058bcdddbe084998f5d0201
Danrlei Hornke <codingdh@gmail.com>
2022-08-31 13:43:54 -0300

    Update logs.controller.ts

--- src/controllers/logs.controller.ts
+++ src/controllers/logs.controller.ts
@@ -6,14 +6,22 @@ export async function ShowLogs(req: Request, res: Response) {
    const { level } = req.query;
    if (!level) return res.status(400).json({ message: "Nível inexistente!" });
    if (level !== "error" && level !== "info") return res.status(400).json({ message: "Nível inexistente!" });
+
+   const stats = fs.statSync(path.join(__dirname, `../logs/${level}.log`));
+   if (!stats.isFile) return res.status(400).json({ message: "Nível inexistente!" });
+
+
    const logs = fs.readFileSync(path.join(__dirname, `../logs/${level}.txt`), "utf-8");
    res.status(200).json({ logs });
 }
 
 export async function FlushLogs(req: Request, res: Response) {
    const { level } = req.query;
-   if(!level) return res.status(400).json({ message: "Nível inexistente!" });
+   if (!level) return res.status(400).json({ message: "Nível inexistente!" });
+   const stats = fs.statSync(path.join(__dirname, `../../logs/${level}.log`));
+   if (!stats.isFile) return res.status(400).json({ message: "Nível inexistente!" });
    if (level !== "error" && level !== "info") return res.status(400).json({ message: "Nível inexistente!" });
    fs.writeFileSync(path.join(__dirname, `../logs/${level}.txt`), "");
    res.status(200).json({ message: "Logs limpos com sucesso!" });
 }
+
