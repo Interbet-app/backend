b25919fe09c2b780f519cb017f4d6b0740d0ea06
Danrlei Hornke <codingdh@gmail.com>
2022-09-14 09:58:15 -0300

    Update index.ts

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -22,7 +22,7 @@ export class File {
 
 export const RefreshOddsPayout = (balance: number[]) => {
    const equilibriumConst = balance[0] + balance[1] + 1000;
-   const marginHouse = 0.3;
+   const marginHouse = 0.35;
    const balanceListDuplicate = [...balance, ...balance];
    const newOdds = [] as any[];
    balance.forEach((bet) => {
