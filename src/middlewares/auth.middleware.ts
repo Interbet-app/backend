86f63287bfe466b8238781247967682998ea7347
Edmar Rocha Alencar <edmaaralencar1@gmail.com>
2023-04-24 21:07:36 -0300

    fix: fix middleware error

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -9,23 +9,23 @@ import { OAuth2Client } from "google-auth-library";
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID as string;
 const googleOAuth = new OAuth2Client(GOOGLE_CLIENT_ID);
 
- export async function AuthUser(req: Request, res: Response, next: any) {
-    try {
-       const token = req.headers["authorization"] as string;
-       if (!token) throw new AppError(422, "Authorization header is required!");
+export async function AuthUser(req: Request, res: Response, next: any) {
+   try {
+      const token = req.headers["authorization"] as string;
+      if (!token) throw new AppError(422, "Authorization header is required!");
       //  Verificar se o token é válido
-       const result = await Jwt.verify(token, next);
-       if (!result) throw new AppError(403, "Authorization token is Invalid!");
+      const result = await Jwt.verify(token, next);
+      if (!result) throw new AppError(403, "Authorization token is Invalid!");
       //  Verificar caso o token seja um token inválido por conta do logout
-       const user = await Cache.get(`${result.userId}`);
-       if (!user && user == token) throw new AppError(403, "Authorization is invalid!");
+      const user = await Cache.get(`${result.userId}`);
+      if (!user && user == token) throw new AppError(403, "Authorization is invalid!");
       //  Carregar validação no payload
-       res.locals.payload = result;
-       next();
-    } catch (error) {
-       next(error);
-    }
- }
+      res.locals.payload = result;
+      next();
+   } catch (error) {
+      next(error);
+   }
+}
 
 export async function AuthGoogle(req: Request, res: Response, next: any) {
    try {
@@ -44,9 +44,10 @@ export async function AuthAdmin(req: Request, res: Response, next: any) {
    try {
       const { method, path } = req;
       const token = Jwt.getLocals(res, next) as Token;
-      if (!token) throw new AppError(403, "Authorization to admin is invalid!");
-      
-      const data = await maintenances.findAll({ where: { userId: token.userId } });
+      const { id } = req.user;
+      if (!id) throw new AppError(403, "Authorization to admin is invalid!");
+
+      const data = await maintenances.findAll({ where: { userId: id } });
       let filter = path.substring(1, path.indexOf("/", 1));
       if (filter.length < 2) filter = path.substring(1);
 
@@ -61,3 +62,4 @@ export async function AuthAdmin(req: Request, res: Response, next: any) {
       next(error);
    }
 }
+
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -8,101 +8,101 @@ const route = Router();
 
 route.get("/bets", Control.GetBets);
 route.post("/bets", AuthMotionUser, Control.CreateBet);
-route.get("/bets/amounts", AuthUser, AuthAdmin, Control.GetBetsSum);
-route.post("/bets/multiple", Middle.MultipleBets, AuthUser, Control.CreateMultipleBets);
+route.get("/bets/amounts", AuthMotionUser, AuthAdmin, Control.GetBetsSum);
+route.post("/bets/multiple", Middle.MultipleBets, AuthMotionUser, Control.CreateMultipleBets);
 route.get("/bets/me", AuthMotionUser, Control.GetUserBets);
-route.get("/bets/game/:id/", Middle.ID, AuthUser, AuthAdmin, Control.GetBetsByGame);
-route.delete("/bets/:id/", Middle.ID, AuthUser, AuthAdmin, Control.DeleteBet);
+route.get("/bets/game/:id/", Middle.ID, AuthMotionUser, AuthAdmin, Control.GetBetsByGame);
+route.delete("/bets/:id/", Middle.ID, AuthMotionUser, AuthAdmin, Control.DeleteBet);
 
 route.get("/games/history", Control.GamesHistory);
 route.get("/games/history/search", Control.GamesHistorySearch);
-route.post("/games/history", AuthUser, AuthAdmin, Middle.CreateGameHistory, Control.GamesHistoryCreate);
-route.delete("/games/history/:id/", Middle.ID, AuthUser, AuthAdmin, Control.GamesHistoryDelete);
+route.post("/games/history", AuthMotionUser, AuthAdmin, Middle.CreateGameHistory, Control.GamesHistoryCreate);
+route.delete("/games/history/:id/", Middle.ID, AuthMotionUser, AuthAdmin, Control.GamesHistoryDelete);
 
 route.get("/games", Control.GetGames);
-route.post("/games", AuthUser, AuthAdmin, Middle.CreateGame, Control.CreateGame);
-route.put("/games", AuthUser, AuthAdmin, Middle.UpdateGame, Control.UpdateGame);
+route.post("/games", AuthMotionUser, AuthAdmin, Middle.CreateGame, Control.CreateGame);
+route.put("/games", AuthMotionUser, AuthAdmin, Middle.UpdateGame, Control.UpdateGame);
 route.get("/games/full", Control.GamesFilter);
 route.get("/games/filter", Control.GamesFilter);
-route.post("/games/process-result/:id/", Middle.ID, Middle.ProcessGame, AuthUser, AuthAdmin, Control.ProcessGame);
+route.post("/games/process-result/:id/", Middle.ID, Middle.ProcessGame, AuthMotionUser, AuthAdmin, Control.ProcessGame);
 route.get("/games/details/:id/", Middle.ID, Control.GameDetails);
 route.get("/games/last/team", Control.TeamLastGames);
 route.get("/games/last/athletic", Control.AthleticLastGames);
 route.get("/games/:id/", Middle.ID, Control.GetGame);
-route.delete("/games/:id/", Middle.ID, AuthUser, AuthAdmin, Control.DeleteGame);
+route.delete("/games/:id/", Middle.ID, AuthMotionUser, AuthAdmin, Control.DeleteGame);
 
 route.get("/odds", Control.GetOdds);
 route.get("/odds/:id/", Middle.ID, Control.GetOdd);
 route.get("/odds/game/:id/", Middle.ID, Control.OddsByGame);
-route.post("/odds", AuthUser, AuthAdmin, Middle.CreateOdd, Control.CreateOdd);
-route.put("/odds", AuthUser, AuthAdmin, Middle.UpdateOdd, Control.UpdateOdd);
-route.delete("/odds/:id/", Middle.ID, AuthUser, AuthAdmin, Control.DeleteOdd);
+route.post("/odds", AuthMotionUser, AuthAdmin, Middle.CreateOdd, Control.CreateOdd);
+route.put("/odds", AuthMotionUser, AuthAdmin, Middle.UpdateOdd, Control.UpdateOdd);
+route.delete("/odds/:id/", Middle.ID, AuthMotionUser, AuthAdmin, Control.DeleteOdd);
 
 route.get("/teams", Control.GetTeams);
 route.get("/teams/:name/", Control.FindTeams);
 route.get("/teams/players/:id/", Middle.ID, Control.TeamPlayers);
 route.get("/teams/info/:id/", Middle.ID, Control.GetTeam);
-route.post("/teams", AuthUser, AuthAdmin, Control.CreateTeam);
-route.post("/teams/players", AuthUser, AuthAdmin, Control.CreatePlayer);
-route.put("/teams", AuthUser, AuthAdmin, Control.UpdateTeam);
-route.put("/teams/players/", AuthUser, AuthAdmin, Control.UpdatePlayer);
-route.delete("/teams/:id/", Middle.ID, AuthUser, AuthAdmin, Control.DeleteTeam);
-route.delete("/teams/players/:id/", Middle.ID, AuthUser, AuthAdmin, Control.DeletePlayer);
+route.post("/teams", AuthMotionUser, AuthAdmin, Control.CreateTeam);
+route.post("/teams/players", AuthMotionUser, AuthAdmin, Control.CreatePlayer);
+route.put("/teams", AuthMotionUser, AuthAdmin, Control.UpdateTeam);
+route.put("/teams/players/", AuthMotionUser, AuthAdmin, Control.UpdatePlayer);
+route.delete("/teams/:id/", Middle.ID, AuthMotionUser, AuthAdmin, Control.DeleteTeam);
+route.delete("/teams/players/:id/", Middle.ID, AuthMotionUser, AuthAdmin, Control.DeletePlayer);
 
-route.get("/players", AuthUser, AuthAdmin, Control.GetPlayers);
+route.get("/players", AuthMotionUser, AuthAdmin, Control.GetPlayers);
 
 route.get("/athletics", Control.GetAthletics);
 route.get("/athletics/:name/", Control.FindAthletics);
-route.post("/athletics", AuthUser, AuthAdmin, Control.CreateAthletic);
-route.put("/athletics", AuthUser, AuthAdmin, Control.UpdateAthletic);
-route.delete("/athletics/:id/", Middle.ID, AuthUser, AuthAdmin, Control.DeleteAthletic);
+route.post("/athletics", AuthMotionUser, AuthAdmin, Control.CreateAthletic);
+route.put("/athletics", AuthMotionUser, AuthAdmin, Control.UpdateAthletic);
+route.delete("/athletics/:id/", Middle.ID, AuthMotionUser, AuthAdmin, Control.DeleteAthletic);
 
 route.get("/events", Control.GetEvents);
-route.post("/events", AuthUser, AuthAdmin, Middle.CreateEvent, Control.CreateEvent);
-route.delete("/events/:id/", AuthUser, AuthAdmin, Control.DeleteEvent);
+route.post("/events", AuthMotionUser, AuthAdmin, Middle.CreateEvent, Control.CreateEvent);
+route.delete("/events/:id/", AuthMotionUser, AuthAdmin, Control.DeleteEvent);
 
 route.get("/adds", Control.GetAdds);
-route.post("/adds", AuthUser, AuthAdmin, Control.CreateAdds);
-route.delete("/adds/:id/", Middle.ID, AuthUser, AuthAdmin, Control.DeleteAdds);
+route.post("/adds", AuthMotionUser, AuthAdmin, Control.CreateAdds);
+route.delete("/adds/:id/", Middle.ID, AuthMotionUser, AuthAdmin, Control.DeleteAdds);
 
-route.get("/user", AuthUser, Control.GetUser);
+route.get("/user", AuthMotionUser, Control.GetUser);
 route.get("/user/all", Control.GetAllUsers);
-route.post("/user/athletic/admin", AuthUser, AuthAdmin, Control.SetAthleticAndTeamAdminId);
+route.post("/user/athletic/admin", AuthMotionUser, AuthAdmin, Control.SetAthleticAndTeamAdminId);
 route.put("/user", AuthMotionUser, Control.UserUpdate);
-// route.put("/user/profile", AuthUser, Control.UserProfile);
+// route.put("/user/profile", AuthMotionUser, Control.UserProfile);
 route.get("/user/me", Control.GetMotionUser);
-route.delete("/user/:id/", AuthMotionUser, Control.DeleteUser);
+// route.delete("/user/:id/", AuthMotionUser, Control.DeleteUser);
 
-route.get("/wallet", AuthUser, Control.GetWallet);
-route.get("/wallet/balances", AuthUser, AuthAdmin, Control.SumBalances);
-route.post("/wallet", AuthUser, AuthAdmin, Control.CreateWallet);
-route.put("/wallet", AuthUser, AuthAdmin, Middle.UpdateWallet, Control.UpdateWallet);
+route.get("/wallet", AuthMotionUser, Control.GetWallet);
+route.get("/wallet/balances", AuthMotionUser, AuthAdmin, Control.SumBalances);
+route.post("/wallet", AuthMotionUser, AuthAdmin, Control.CreateWallet);
+route.put("/wallet", AuthMotionUser, AuthAdmin, Middle.UpdateWallet, Control.UpdateWallet);
 
 // route.post("/google/oauth", Middle.GoogleOAuthSchema, AuthGoogle, Control.GoogleOAuth);
 // route.post("/facebook/oauth", Middle.FacebookOAuthSchema, Control.FacebookOAuth);
 // route.post("/instagram/oauth", Middle.InstagramOAuthSchema, Control.InstagramOAuth);
-route.get("/logout", AuthUser, Control.Logout);
+route.get("/logout", AuthMotionUser, Control.Logout);
 
 //- Notificações
-route.get("/notifications", AuthUser, Control.UserNotifications);
-route.put("/notifications/:id/", Middle.ID, AuthUser, Control.NotificationMarkAsRead);
-route.delete("/notifications/:id/", Middle.ID, AuthUser, Control.NotificationDelete);
+route.get("/notifications", AuthMotionUser, Control.UserNotifications);
+route.put("/notifications/:id/", Middle.ID, AuthMotionUser, Control.NotificationMarkAsRead);
+route.delete("/notifications/:id/", Middle.ID, AuthMotionUser, Control.NotificationDelete);
 
 //- Administração
-route.get("/maintenances/me", AuthUser, Control.UserMaintenances);
-route.get("/maintenances", AuthUser, AuthAdmin, Control.GetMaintenances);
-route.get("/maintenances/:group/", AuthUser, AuthAdmin, Control.FindGroupMaintenances);
-route.post("/maintenances", AuthUser, AuthAdmin, Middle.CreateMaintenance, Control.CreateMaintenance);
-route.delete("/maintenances/:id/", Middle.ID, AuthUser, AuthAdmin, Control.DeleteMaintenance);
+route.get("/maintenances/me", AuthMotionUser, Control.UserMaintenances);
+route.get("/maintenances", AuthMotionUser, AuthAdmin, Control.GetMaintenances);
+route.get("/maintenances/:group/", AuthMotionUser, AuthAdmin, Control.FindGroupMaintenances);
+route.post("/maintenances", AuthMotionUser, AuthAdmin, Middle.CreateMaintenance, Control.CreateMaintenance);
+route.delete("/maintenances/:id/", Middle.ID, AuthMotionUser, AuthAdmin, Control.DeleteMaintenance);
 
 //-Depósitos
-route.get("/deposits", AuthUser, Control.UserDeposits);
-route.get("/deposits/:id/", Middle.ID, AuthUser, Control.UserDepositDetails);
-route.post("/deposits", Middle.CreateDeposit, AuthUser, Control.CreateDeposit);
+route.get("/deposits", AuthMotionUser, Control.UserDeposits);
+route.get("/deposits/:id/", Middle.ID, AuthMotionUser, Control.UserDepositDetails);
+route.post("/deposits", Middle.CreateDeposit, AuthMotionUser, Control.CreateDeposit);
 
 //-Retiradas
-route.get("/withdrawals", AuthUser, Control.UserWithdrawals);
-route.post("/withdrawals", AuthUser, Control.CreateWithdrawal);
+route.get("/withdrawals", AuthMotionUser, Control.UserWithdrawals);
+route.post("/withdrawals", AuthMotionUser, Control.CreateWithdrawal);
 
 //- Classificações
 route.get("/ranking/bets", Control.UsersBetsRanking);
@@ -110,12 +110,12 @@ route.get("/ranking/athletics", Control.AthleticsRanking);
 route.get("/ranking/:id/", Middle.ID, Control.EventRanking);
 
 //- Logs
-route.get("/logs", AuthUser, AuthAdmin, Control.ShowLogs);
-route.post("/logs/flush", AuthUser, AuthAdmin, Control.FlushLogs);
+route.get("/logs", AuthMotionUser, AuthAdmin, Control.ShowLogs);
+route.post("/logs/flush", AuthMotionUser, AuthAdmin, Control.FlushLogs);
 
 //- Feedback
-route.get("/feedback", AuthUser, AuthAdmin, Control.GetAllFeedbacks);
-route.post("/feedback", AuthUser, Control.CreateFeedback);
+route.get("/feedback", AuthMotionUser, AuthAdmin, Control.GetAllFeedbacks);
+route.post("/feedback", AuthMotionUser, Control.CreateFeedback);
 
 //- Callback routes
 route.post("/deposits/complete/callback", Control.OpenPixCallbackComplete);
