55add8f67f58e4a641ba1984d77e3af7164b2c31
Danrlei Hornke <codingdh@gmail.com>
2023-05-30 19:41:58 -0300

    :fire: create developer stage

--- src/services/betmotion/api/index.ts
+++ src/services/betmotion/api/index.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const Betmotion = axios.create({
-   baseURL: "https://api.betmotion.com",
+const BETMOTION_API_URL = process.env.BETMOTION_API_URL as string;
+
+export default axios.create({
+   baseURL: BETMOTION_API_URL,
    headers: {
       "Content-Type": "text/xml",
    },
 });
-
-export default Betmotion;
