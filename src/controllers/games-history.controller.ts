6dfbe7c7adc733ff48f22d28b2b9a4ae8aba19d9
Danrlei Hornke <codingdh@gmail.com>
2023-06-14 10:10:12 -0300

    :recycle: add team picture on games history route

--- src/controllers/games-history.controller.ts
+++ src/controllers/games-history.controller.ts
@@ -1,22 +1,24 @@
-import {NextFunction, Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { Op } from "sequelize";
-import { gamesHistory } from "../models";
+import { ITeamModel, gamesHistory, teams } from "../models";
 import { IGameHistory } from "../interfaces";
 
 export async function GamesHistory(_req: Request, res: Response, next: NextFunction) {
    try {
-      gamesHistory.findAll()
-         .then(response => res.status(200).json(response))
-         .catch(err => console.log(err))
+      gamesHistory
+         .findAll()
+         .then((response) => res.status(200).json(response))
+         .catch((err) => console.log(err));
    } catch (error) {
       next(error);
    }
 }
 export async function GamesHistorySearch(req: Request, res: Response, next: NextFunction) {
    try {
-      const { event, teamA, teamB } = req.query;
-      const history = await gamesHistory.findAll({
+      const { event, teamA, teamB, gender } = req.query;
+      const games = await gamesHistory.findAll({
          where: {
+            gender: gender as string,
             [Op.or]: [
                { event: { [Op.like]: `%${event}%` } },
                { teamA: { [Op.like]: `%${teamA}%` } },
@@ -24,7 +26,30 @@ export async function GamesHistorySearch(req: Request, res: Response, next: Next
             ],
          },
       });
-      const response = history.map((item: IGameHistory) => item as IGameHistory);
+
+      const team = await teams.findAll({
+         where: {
+            [Op.or]: [
+               { name: { [Op.like]: `%${teamA}%` } },
+               { name: { [Op.like]: `%${teamB}%` } },
+               { abbreviation: { [Op.like]: `%${teamA}%` } },
+               { abbreviation: { [Op.like]: `%${teamB}%` } },
+            ],
+         },
+      });
+      const history = games.map((item: IGameHistory) => item as IGameHistory);
+      const pictures = team.map((item: any) => item as ITeamModel);
+
+      const response = history.map((item: IGameHistory) => {
+         const teamA = pictures.find((team) => team.name === item.teamA || team.abbreviation === item.teamA);
+         const teamB = pictures.find((team) => team.name === item.teamB || team.abbreviation === item.teamB);
+         return {
+            ...item,
+            teamA: teamA?.picture,
+            teamB: teamB?.picture,
+         };
+      });
+
       res.status(200).json(response);
    } catch (error) {
       next(error);
