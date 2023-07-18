97a6f765c1d090226de1bfcd32cde0700d54a112
Danrlei Hornke <dscoding.br@gmail.com>
2023-07-18 16:16:26 -0300

    :bug: fix  types error

--- src/controllers/events.controller.ts
+++ src/controllers/events.controller.ts
@@ -154,7 +154,7 @@ export async function GetEventClassification(req: Request, res: Response, next: 
 
 export async function CreateEvent(req: Request, res: Response, next: NextFunction) {
    try {
-      const { name, description, title, location, startDate, endDate, type } = req.body;
+      const { name, description, title, location, startDate, endDate, type, gender } = req.body;
       const event = await events.create({
          name,
          description,
@@ -162,6 +162,7 @@ export async function CreateEvent(req: Request, res: Response, next: NextFunctio
          location,
          startDate,
          endDate,
+         gender,
          type,
          createdAt: new Date(),
          updatedAt: new Date(),
